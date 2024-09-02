931d6e9b45f121cef24d25f9c8d27e2604d4b88f
little1d <2431542525@qq.com>
2024-09-02 17:16:56 +0800

    feat(config): add config and file_operation

---
+++ swchatbot/config.py
@@ -0,0 +1,5 @@
+class Config:
+    embedding_model_path = "maidalun1020/bce-embedding-base_v1"
+    reranker_model_path = "maidalun1020/bce-reranker-base_v1"
+    work_dir = 'workdir'
+    repo_dir = 'repodir'
